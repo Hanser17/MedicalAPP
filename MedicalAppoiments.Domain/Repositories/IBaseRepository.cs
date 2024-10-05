40ff5dadf29544f286d6bec2f438244014f61fb2
Hanser17 <Hanser.pimentel@gmail.com>
2024-10-05 14:00:41 -0400

    created IBaseRepository

--- MedicalAppoiments.Domain/Repositories/IBaseRepository.cs
+++ MedicalAppoiments.Domain/Repositories/IBaseRepository.cs
@@ -1,10 +1,23 @@
 ﻿
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace MedicalAppoiments.Domain.Repositories
 {
-    internal interface IBaseRepository
+    
+    public interface IBaseRepository <TEntity> where TEntity : class
     {
+     
+        Task Save(TEntity entity);
+      
+        Task Update (TEntity entity);
+       
+        Task Remove(TEntity entity);
+       
+        List <TEntity> GetAll ();
+       
+        TEntity GetEntityBy(Type Id);
+        bool Exists(Expression<Func<TEntity, bool>> filter);
 
     }
 }
